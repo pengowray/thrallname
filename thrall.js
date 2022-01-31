a0abb566b71af1fed286ce523849be8c083cf186
Pengo Wray <peter@becauseofgames.com>
2022-01-31 16:05:42 +1100

    tidy up code; add name stats

--- thrall.js
+++ thrall.js
@@ -1,15 +1,18 @@
 // thrall name gen
 
-var MascFirst = "A Ab Ac Ad Ae Af Ain Al Alc Ald Alf An Ash At Ath Aud Ave Bax Ben Bent Beo Bo Brad Brin Bron Cas Ceo Chad Ches Clay Cliff Clint Col Com Cor Craw Cuth Dal Dar Dex Di Dud Dun Ea Ead Eal Eald Eb Ed Ei Eid El Elf Eo Es Eth Ever Fin Ful Gal Gi Glad God Hal Hay Hayd Haw Hux In Jon Kal Ken Kil Kith La Lal Lay Les Lie Lin Na Nal Nes Nor Oak Os Pais Pend Rad San Thor Thorn Tal Tam Tru Ul Up Ur Uth Van Wal Ward Wayn Weald Wes Whit Wil Wilk Wini Wins Wolf Wood Wright Wy Yar".split(" ");
-var FemFirst = "A Ab Ac Ad Ae Af Ain Al Alc Ald Alf An Ash At Ath Aud Ave Ben Bent Beo Bev Bever Brin Bron Cas Ceo Ches Col Com Cor Craw Cuth Dal Dar Di Dud Dun Ea Ead Eal Eald Eb Ed Ei Eid El Elf Eo Es Eth Ever Fin Ful Gal Gi Glad God Hal Hay Hayd Haw In Jon Kal Ken Kil Kith La Lal Lay Les Lie Lin Na Nal Nes Nor Oak Os Pais Pend Rad San Tal Tam Tru Ul Up Ur Uth Van Wal Ward Wayn Weald Wes Whit Wil Wilk Wini Wins Wolf Wood Wright Wy Yar".split(" ");
-var MascSecond = "a al ard be bert dair den en er field ford gar gard hard helm here ie ion is land ly man mar mer mere mon mond on ran red ric rith seth sly son stan ter ton um us vard ven wald ward wart werd wick wig win wulf".split(" ");
-var FemSecond = "a ba be da ege en ette ia ie is ith lin low ly lyn me mera mere nne ra reda rey ril rith ta vena wina ytha".split(" ");
-var MascSimple = "Ash Aspen Aster Barrow Basil Bay Bear Beryl Birch Bird Blake Blaze Blue Breeze Briar Brick Carnelian Cedar Cloud Copper Cove Dusk Eagle Elm Ember Field Flint Forest Fox Gale Glen Gray Green Grove Hawk Heath Heron Jasper Jay Jet Kale Lake Marsh Mica Ocean Onyx Rain Red Reed Ridge River Rowan Silver Sky Slate Sorrel Sparrow Stone Stone Storm Summer Winter Woad Woad Yarrow".split(" ");
-var FemSimple = "Amber Anise Ash Aspen Aster Autumn Balsam Beryl Bird Blake Blue Bramble Breeze Briar Brook Camellia Carmine Clementine Clover Crystal Dahlia Daisy Dawn Dove Ebony Elm Ember Emerald Eve Fern Field Fox Gale Ginger Harvest Hazel Holly Hyacinth Iris Ivory Ivy Jade Jade Jasmine Jewel Juniper Lake Laurel Lavender Lily Magnolia Maple Marigold Meadow Mica Mist Moss Myrtle Olive Opal Parsley Pearl Pepper Poppy Primrose Rain Red Reed River Rose Rosemary Rowan Ruby Rue Rush Sage Sapphire Scarlet Silver Sky Sorrel Sparrow Star Storm Summer Violet Violet Willow Wisteria Wren".split(" ");
-var MascBrairi = "Ako Adrian Afynasi Ajdin Akim Aldin Aleksi Alem Anatoli Andel Andon Andrez".split(" ");
-var FemBrairi = "Ada Adalejda Adrianja Agatka Aglaja Agniezka Ajda Akilina Aksinja Aldinja Aleksinja Alenka Amalija Ana Anda".split(" ");
+var MascFirst = "A Ab Ac Ad Ae Af Ain Al Alc Ald Alf An Ash At Ath Aud Ave Bax Ben Bent Beo Bo Brad Brin Bron Cas Ceo Chad Ches Clay Cliff Clint Col Com Cor Craw Cuth Dal Dar Dex Di Dud Dun Ea Ead Eal Eald Eb Ed Ei Eid El Elf Eo Es Eth Ever Fin Ful Gal Gi Glad God Hal Hay Hayd Haw Hux In Jon Kal Ken Kil Kith La Lal Lay Les Lie Lin Na Nal Nes Nor Oak Os Pais Pend Rad San Thor Thorn Tal Tam Tru Ul Up Ur Uth Van Wal Ward Wayn Weald Wes Whit Wil Wilk Wini Wins Wolf Wood Wright Wy Yar".split(" ").sort();
+var FemFirst = "A Ab Ac Ad Ae Af Ain Al Alc Ald Alf An Ash At Ath Aud Ave Ben Bent Beo Bev Bever Brin Bron Cas Ceo Ches Col Com Cor Craw Cuth Dal Dar Di Dud Dun Ea Ead Eal Eald Eb Ed Ei Eid El Elf Eo Es Eth Ever Fin Ful Gal Gi Glad God Hal Hay Hayd Haw In Jon Kal Ken Kil Kith La Lal Lay Les Lie Lin Na Nal Nes Nor Oak Os Pais Pend Rad San Tal Tam Tru Ul Up Ur Uth Van Wal Ward Wayn Weald Wes Whit Wil Wilk Wini Wins Wolf Wood Wright Wy Yar".split(" ").sort();
+var MascSecond = "a al ard be bert dair den en er field ford gar gard hard helm here ie ion is land ly man mar mer mere mon mond on ran red ric rith seth sly son stan ter ton um us vard ven wald ward wart werd wick wig win wulf".split(" ").sort();
+var FemSecond = "a ba be da ege en ette ia ie is ith lin low ly lyn me mera mere nne ra reda rey ril rith ta vena wna ytha".split(" ").sort();
+var MascSimple = "Ash Aspen Aster Barrow Basil Bay Bear Beryl Birch Bird Blake Blaze Blue Breeze Briar Brick Carnelian Cedar Cloud Copper Cove Dusk Eagle Elm Ember Field Flint Forest Fox Gale Glen Gray Green Grove Hawk Heath Heron Jasper Jay Jet Kale Lake Marsh Mica Ocean Onyx Rain Red Reed Ridge River Rowan Silver Sky Slate Sorrel Sparrow Stone Stone Storm Summer Winter Woad Woad Yarrow".split(" ").sort();
+var FemSimple = "Amber Anise Ash Aspen Aster Autumn Balsam Beryl Bird Blake Blue Bramble Breeze Briar Brook Camellia Carmine Clementine Clover Crystal Dahlia Daisy Dawn Dove Ebony Elm Ember Emerald Eve Fern Field Fox Gale Ginger Harvest Hazel Holly Hyacinth Iris Ivory Ivy Jade Jade Jasmine Jewel Juniper Lake Laurel Lavender Lily Magnolia Maple Marigold Meadow Mica Mist Moss Myrtle Olive Opal Parsley Pearl Pepper Poppy Primrose Rain Red Reed River Rose Rosemary Rowan Ruby Rue Rush Sage Sapphire Scarlet Silver Sky Sorrel Sparrow Star Storm Summer Violet Violet Willow Wisteria Wren".split(" ").sort();
+var MascBrairi = "Ako Adrian Afynasi Ajdin Akim Aldin Aleksi Alem Anatoli Andel Andon Andrez".split(" ").sort();
+var FemBrairi = "Ada Adalejda Adrianja Agatka Aglaja Agniezka Ajda Akilina Aksinja Aldinja Aleksinja Alenka Amalija Ana Anda".split(" ").sort();
 
 
+
+function intersect_safe(a, b) { 
+//via: https://stackoverflow.com/a/1885660
 /* finds the intersection of 
  * two arrays in a simple fashion.  
  *
@@ -22,13 +25,10 @@ var FemBrairi = "Ada Adalejda Adrianja Agatka Aglaja Agniezka Ajda Akilina Aksin
  *  Should have O(n) operations, where n is 
  *    n = MIN(a.length(), b.length())
  */
-function intersect_safe(a, b)
-{
   var ai=0, bi=0;
   var result = [];
 
-  while( ai < a.length && bi < b.length )
-  {
+  while( ai < a.length && bi < b.length ) {
      if      (a[ai] < b[bi] ){ ai++; }
      else if (a[ai] > b[bi] ){ bi++; }
      else /* they're equal */
@@ -66,8 +66,12 @@ Cat_Simple = "S";
 Cat_Brairi = "B"
 
 function generate(gender = Gender_Either, cat = Cat_Random) {
-	gen.innerText = GenName(gender, cat);
-	//gen.value = GenName(gender, cat); // if want to fill text input: <input class="inp" id="gen" type="text">
+	setName(GenName(gender, cat));
+}
+
+function setName(text) {
+	gen.innerText = text;
+	//gen.value = GenName(gender, cat); // if want to fill a text input box like: <input class="inp" id="gen" type="text">
 }
 
 function GenName(gender = Gender_Either, cat = Cat_Random) {
@@ -115,18 +119,21 @@ function GenName(gender = Gender_Either, cat = Cat_Random) {
 	return "Error"; //return $"Error [{gender} {cat}]";
 }
 
-/*
+
 function Stats() {
-	return $"M: S:{MascSimple}, 1:{MascFirst}, 2:{MascSecond}, B:{MascBrairi}\n" +
-		$"F: S:{FemSimple}, 1:{FemFirst}, 2:{FemSecond}, B:{FemBrairi}\n" +
-		$"N: S:{NeutralSimple}, 1:{NeutralFirst}, 2:{NeutralSecond}, B:0";
+	return "Name stats:\n" +
+		`[♂]: S:${MascSimple.length}, 1:${MascFirst.length}, 2:${MascSecond.length}, B:${MascBrairi.length};\n` +
+		`[♀]: S:${FemSimple.length}, 1:${FemFirst.length}, 2:${FemSecond.length}, B:${FemBrairi.length};\n` +
+		`[N]: S:${NeutralSimple.length}, 1:${NeutralFirst.length}, 2:${NeutralSecond.length}, B:${NeutralBrairi.length};`;
 }
-*/
+
+function CreateStatsComment() {
+	const comment = document.createComment(Stats());
+	document.body.appendChild(comment);
+}
 
 
-
-function PickOne(items)
-{
+function PickOne(items) {
     return items[Math.floor(Math.random()*items.length)];
 }
 
